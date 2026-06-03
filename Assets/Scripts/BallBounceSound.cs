9e3a47dc8897a53c274df40b1451700a2f5ff853
PetraTurkovic <petra.turkovic@fer.unizg.hr>
2026-06-03 13:50:18 +0200

    BetterSound

--- Assets/Scripts/BallBounceSound.cs
+++ Assets/Scripts/BallBounceSound.cs
@@ -4,6 +4,7 @@ public class BallBounceSound : MonoBehaviour
 {
     public AudioClip[] bounce_clips;
     private AudioSource audioSource;
+    public float maxImpactVelocity = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,11 +13,15 @@ public class BallBounceSound : MonoBehaviour
 
     // Update is called once per frame
     void OnCollisionEnter(Collision col){
+        float impact = col.relativeVelocity.magnitude;
+        float volume = Mathf.Clamp01(impact / maxImpactVelocity);
+        //ako su mali udarci
+        if (volume < 0.1f) return;
         int index = Random.Range(0, bounce_clips.Length);
 
         //change pitch
         audioSource.pitch = Random.Range(0.9f, 1.1f);
 
-        audioSource.PlayOneShot(bounce_clips[index]);
+        audioSource.PlayOneShot(bounce_clips[index], volume);
     }
 }
